e73e427606cf9b498aa00a80628e7ec958a2b66a
Esther Eze <43142258+akowe@users.noreply.github.com>
2022-09-08 11:11:57 +0100

    Revert "pull"

--- app/Country.php
+++ app/Country.php
@@ -3,14 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
+
 class Country extends Model
 {
     //
      protected $table ='country';
-
-     function get_country_code($country){
-        $getCountryCode= DB::table($this->table)->where('country', $country)->first();
-        return $getCountryCode->country_code;
-    }
 }
