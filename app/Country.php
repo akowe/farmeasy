de28b799430b068322ab3badc20fc6c8fcbf284e
Mutiu Adepoju <methyl2007@yahoo.com>
2022-09-11 23:01:36 -0400

    i also added the ResponseBuilder class for the json format to the country model class

--- app/Country.php
+++ app/Country.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use App\Http\Helper\ResponseBuilder;
+
 class Country extends Model
 {
     //
@@ -14,7 +16,12 @@ class Country extends Model
         if($getCountryCode){
             return $getCountryCode->country_code;
         }else{
-            return response()->json(["message"=>"This application is not allowed in ".ucfirst($country)]);
+            $status = false;
+            $message ="This application is not allowed in".ucfirst($country);
+            $error = '';
+            $code = 400;     
+            ResponseBuilder::result($status, $message, $error, $code);               
+            
         }
         
     }
