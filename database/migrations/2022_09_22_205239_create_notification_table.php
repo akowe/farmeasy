f85928ffa4c136677881b3b6a37da3ea1b0631d1
estherezeakowe <estherakowe@yahoo.com>
2022-09-22 22:01:31 +0100

    notification
    notification table

---
+++ database/migrations/2022_09_22_205239_create_notification_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('request_id');
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('description');
+            $table->string('notice_status');
+
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notification');
+    }
+}
---
+++ database/migrations/2022_09_22_205255_create_payment_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('request_id');
+            $table->string('pay_reference');
+            $table->string('pay_status');
+            $table->string('gateway_ref');
+            $table->string('pay_date');
+            $table->string('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment');
+    }
+}
